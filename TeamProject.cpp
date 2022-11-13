7747d1d26d69fda8739dd08079814a09f90c09ec
aleksey laikachev <super.games29@yandex.ru>
2022-11-13 23:09:49 +0300

    Добавил файл с главной функцией main

---
+++ TeamProject.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <Windows.h>       //для SetConsoleCP() и SetConsoleOutputCP()
+#include "ClassUserInterface.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	UserInterface theUserInterface;
+	theUserInterface.interact();
+	return 0;
+}
